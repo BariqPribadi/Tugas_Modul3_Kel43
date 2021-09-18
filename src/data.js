924bbb9a0e65bd88e83f4766f7e07732bad4d99f
BariqPribadi <50965999+BariqPribadi@users.noreply.github.com>
2021-09-18 20:36:10 +0700

    First Commit

---
+++ src/data.js
@@ -0,0 +1,58 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Indomie Goreng",
+    img: "img/1.jpg",
+    price: 2500,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Telur (per ons)",
+    img: "img/2.jpg",
+    price: 1200,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Permen",
+    img: "img/3.jpg",
+    price: 2000,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Cheetos",
+    img: "img/4.jpg",
+    price: 3000,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Beras 5KG",
+    img: "img/5.jpg",
+    price: 100000,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+];
+
+export const detailProduct = {
+    id: 0,
+    title: 0,
+    img: 0,
+    price: 0,
+    inCart: false,
+    count: 0,
+    total: 0
+};
